36450245f4770012f0b1c3d9a22d54698c81d1e3
Gabriel Amoedo <gbas95@outlook.com>
2020-12-09 10:23:41 -0300

    Atualizando projeto com testes de rotas

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+Route::get('/produtos/{idProduto}', function ($idProduto) {
+    return "Lista de produtos: {$idProduto}";
+});
+
+
+Route::get('/prodam', function () {
+    return "Cheguei na rota da Prodam";
+});
+
+Route::get('/teste', function () {
+    return "Cheguei no teste";
+});
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/view2', function () {
+    return view('welcome');
+});
+
+Route::match(['get','post'], '/certeira', function () {
+    return "Deu Match";
 });
