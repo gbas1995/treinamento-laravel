1a45811e0f1768141eb8b8909ed22135fdbb4440
Marcio Amoedo <amoedo@prodam.am.gov.br>
2020-12-09 11:25:39 -0400

    Papai que colocou

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Exemplo 3 de redirecionamento
+Route::get('/redirecionamento3', function() {
+    return redirect()->route('url.nova');
+});
+    
+/* Exemplo 2 de redirecionamento
+Route::redirect('rota_antiga', 'rota_nova');
+*/
+    
+// Exemplo 1 de redirecionamento
+Route::get('/rota_antiga/{param}', function($param) {
+    return redirect('rota_nova/' . $param);
+});
+        
+Route::get('/rota_nova', function() {
+   return 'rota_nova!';
+})->name('url.nova');
+// Papai que colocou ..        
 
 Route::get('/produtos/{idProduto}', function ($idProduto) {
     return "Lista de produtos: {$idProduto}";
