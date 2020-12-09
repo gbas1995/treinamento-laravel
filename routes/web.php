41bf31b8404e5ef634d7cf6d5fc422eeae217650
Gabriel Amoedo <gbas95@outlook.com>
2020-12-09 12:28:29 -0300

    Atualizando projeto com testes de rotas

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,30 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/produtos/{idProduto}', function ($idProduto) {
-    return "Lista de produtos: {$idProduto}";
+//exemplo 3 de redirecionamento
+
+Route:: get('/redirecionamento3', function(){
+	return redirect()->route('url.nova');
 });
+
+/*//Exemplo 2 de redirecinamento
+Route:: redirect('rota_antiga', 'rota_nova'); */
+
+//exemplo 1 de redirecinamento
+
+/*Route:: get('/rota_antiga/{param}', function($param){
+	return redirect("/rota_nova/" .  $param);
+
+}); */
+
+
+Route::get('/rota_nova', function(){
+	return "Rota nova!";
+}) ->name('url.nova');
+
+/*Route::get('/produtos/{idProduto?}', function ($idProduto = "") {
+    return "Lista de produtos: {$idProduto}";
+});*/
 
 
 Route::get('/prodam', function () {
